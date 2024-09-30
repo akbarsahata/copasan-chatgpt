1ef46c5a423d39f80b44acd3a6f853fc385aeee2
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2024-09-30 22:55:55 +0700

    Refactor static file serving to check same domain before allowing access

--- api/index.js
+++ api/index.js
@@ -5,7 +5,20 @@ const handlers = require("../handlers/");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// Middleware to check the Referer or Origin header
+const forbiddenFiles = ["metadata.json"];
+
+function checkSameDomain(req, res, next) {
+    const requestedFile = path.basename(req.path);
+
+    if (forbiddenFiles.includes(requestedFile)) {
+        res.status(403).send("Forbidden"); // Deny the request
+    } else {
+        next(); // Allow the request to continue
+    }
+}
+
+app.use(checkSameDomain, express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", handlers.home);
 
