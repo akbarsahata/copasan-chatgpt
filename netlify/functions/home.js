7c63fd0d31aa7a45f69a4bcd63e79022cfc05206
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-10-02 11:15:38 +0700

    Refactor home.js for improved code consistency and readability; update error messages and enhance search functionality

--- netlify/functions/home.js
+++ netlify/functions/home.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 
 // Add fetch polyfill for Node.js environments that don't have it
 if (!global.fetch) {
-  global.fetch = require('node-fetch');
+  global.fetch = require("node-fetch");
 }
 
 exports.handler = async (event, context) => {
   try {
     let metadata;
-    
+
     // Try to read from the local file system first (for local development)
     try {
       const metadataPath = path.join(process.cwd(), "metadata.json");
@@ -19,10 +19,10 @@ exports.handler = async (event, context) => {
       // If local file doesn't exist, fetch from the deployed site
       const baseUrl = process.env.URL || `https://${event.headers.host}`;
       const response = await fetch(`${baseUrl}/metadata.json`);
-      if (!response.ok) throw new Error('Metadata not found');
+      if (!response.ok) throw new Error("Metadata not found");
       metadata = await response.json();
     }
-    
+
     const fileList = Object.keys(metadata)
       .map((file) => {
         const { title, desc, createdAt } = metadata[file];
@@ -49,37 +49,40 @@ exports.handler = async (event, context) => {
       });
 
     const siteTitle = "Akbar Sahata's Blog";
-    const description = "Welcome to my personal blog where I share my thoughts, opinions, and a comprehensive archive of my interactions with ChatGPT. Dive into the knowledge and fun!";
+    const description =
+      "Welcome to my personal blog where I share my thoughts, opinions, and a comprehensive archive of my interactions with ChatGPT. Dive into the knowledge and fun!";
     const imageUrl = "/image.webp";
     const pageUrl = `https://${event.headers.host}${event.path}`;
-    
+
     const html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta name="description" content="${description}">
-          <meta property="og:title" content="${siteTitle}">
-          <meta property="og:description" content="${description}">
-          <meta property="og:image" content="${imageUrl}">
-          <meta property="og:url" content="${pageUrl}">
-          <meta property="og:type" content="website">
-          <link rel="icon" href="/favicon.ico" type="image/x-icon">
-          <title>${siteTitle}</title>
-          <style>
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="${description}">
+            <meta property="og:title" content="${siteTitle}">
+            <meta property="og:description" content="${description}">
+            <meta property="og:image" content="${imageUrl}">
+            <meta property="og:url" content="${pageUrl}">
+            <title>${siteTitle}</title>
+            <style>
           body {
             font-family: Arial, sans-serif;
             background-color: #f0f2f5;
             color: #333;
             margin: 0;
             padding: 0;
+            width: 100%;
+            height: 100vh;
+            overflow-x: hidden; /* Prevent horizontal overflow */
           }
           .container {
-            padding: 20px 40px;
+            padding: 20px;
             max-width: 100%;
             width: 100%;
             text-align: left;
+            box-sizing: border-box; /* Include padding in width calculation */
           }
           h1 {
             color: #007acc;
@@ -93,47 +96,129 @@ exports.handler = async (event, context) => {
           .card {
             background-color: #fff;
             border: 1px solid #ddd;
-            border-radius: 4px;
+            border-radius: 8px;
             padding: 20px;
             margin-bottom: 20px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
           }
           .card h2 {
-            margin-top: 0;
+            font-size: 24px;
             color: #007acc;
+            margin: 0 0 10px 0;
           }
-          .card a {
+          .card p {
+            font-size: 16px;
+            color: #333;
+            margin: 0 0 10px 0;
+          }
+          a {
+            color: #1e90ff;
             text-decoration: none;
-            color: inherit;
+            font-weight: bold;
           }
-          .card a:hover h2 {
+          a:hover {
             text-decoration: underline;
           }
           .card-footer {
-            margin-top: 15px;
-            font-size: 14px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 10px;
+          }
+          .card-footer small {
+            display: block;
+            margin-top: 10px;
             color: #666;
           }
-          .card-footer .date {
-            margin-right: 15px;
+          .search-container {
+            margin: 20px auto;
+            max-width: 100vw;
           }
-          .card-footer a {
+          .search-container input {
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            width: 100%; /* Ensure the input takes the full width of its container */
+            box-sizing: border-box; /* Include padding and border in the element's total width */
+          }
+          .search-results {
+            margin-top: 20px;
+          }
+          .search-results .result-item {
+            padding: 10px;
+            border-bottom: 1px solid #ddd;
+          }
+          .search-results .result-item a {
+            font-size: 18px;
             color: #007acc;
-            text-decoration: none;
           }
-          .card-footer a:hover {
+          .search-results .result-item p {
+            font-size: 14px;
+            color: #333;
+            margin: 5px 0 5px 0;
+          }
+          .search-results .result-item a:hover {
             text-decoration: underline;
           }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <h1>${siteTitle}</h1>
-            <p>${description}</p>
-            ${fileList.join('')}
+            </style>
+          </head>
+          <body>
+            <div class="container">
+          <h1>Welcome to ${title}!</h1>
+          <p>${description}</p>
+          <div class="search-container">
+            <input type="text" id="search-input" placeholder="Search posts...">
           </div>
-          <script id="dsq-count-scr" src="//akbarsahata.disqus.com/count.js" async></script>
-        </body>
-        </html>`;
+          <div class="search-results" id="search-results"></div>
+          <div class="card-container">${fileList.join("")}</div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/fuse.js@6.4.6"></script>
+            <script>
+              document.querySelectorAll('small.date').forEach((element) => {
+                const date = new Date(element.textContent.replace('Created at: ', ''));
+                element.textContent = 'Created at: ' + date.toLocaleString();
+              });
+
+              const searchInput = document.getElementById('search-input');
+              const searchResults = document.getElementById('search-results');
+
+              fetch('/metadata.json')
+                .then(response => response.json())
+                .then(result => Object.keys(result)
+                  .map(fileName => ({
+                    file: fileName,
+                    title: result[fileName].title,
+                    desc: result[fileName].desc
+                  }))
+                )
+                .then(data => {
+                  const fuse = new Fuse(data, {
+                    keys: ['title', 'desc'],
+                    minMatchCharLength: 3,
+                    distance: 500,
+                  });
+
+                  searchInput.addEventListener('input', () => {
+                    const searchTerm = searchInput.value;
+                    const results = fuse.search(searchTerm);
+
+                    searchResults.innerHTML = results.map(result => {
+                      const { file, title, desc } = result.item;
+                      return \`
+                    <div class="result-item">
+                      <a href="/articles/\${file}">\${title}</a>
+                      <p>\${desc}</p>
+                    </div>
+                      \`;
+                    }).join('');
+                  });
+                });
+            </script>
+            <script id="dsq-count-scr" src="//copasan-chatgpt.disqus.com/count.js" onload="DISQUSWIDGETS.getCount({reset: true});" async></script>
+          </body>
+          </html>
+        `;
 
     return {
       statusCode: 200,
